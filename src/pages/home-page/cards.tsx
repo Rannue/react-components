fea12f6a60aa6f3db662d49e9f3e2b32d0c675bd
Anna <yavianbo@gmail.com>
2023-04-02 16:43:27 +0300

    feat: update home page with hooks

--- src/pages/home-page/cards.tsx
+++ src/pages/home-page/cards.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IProduct } from '../../assets/data';
 import { CardItem } from './card-item';
 
-type TCardsState = { cardsArr: IProduct[] };
+export const Cards: React.FC<IProduct[]> = (props) => {
+  const [cardsArr] = useState<IProduct[]>(Object.values(props));
 
-export class Cards extends React.Component<IProduct[], TCardsState> {
-  constructor(props: Readonly<IProduct[]> | IProduct[]) {
-    super(props);
-    this.state = {
-      cardsArr: Object.values(this.props),
-    };
-  }
-
-  render() {
-    return (
-      <div className="card-container">
-        {this.state.cardsArr.map((card) => (
-          <div className="card" key={card.id}>
-            <CardItem {...card} />
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="card-container">
+      {cardsArr.map((card) => (
+        <div className="card" key={card.id}>
+          <CardItem {...card} />
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { products } from '../../assets/data';
 import { Cards } from './cards';
 import { SearchBar } from './search-bar';
 import '../style.css';
 
 const HomePage = () => {
+  const [cardsArr, setCardsArr] = useState(products);
+
+  useEffect(() => {
+    setCardsArr(Object.values(products));
+  }, []);
+
   return (
     <>
       <div className="search-bar__wrapper">
         <h2 data-testid="main-page">Main page</h2>
         <SearchBar />
       </div>
-      <Cards {...products} />
+      <Cards {...cardsArr} />
     </>
   );
 };
